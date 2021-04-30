758420a7f0bb2ee2681da1eb7b98ae98ec7eaf5e
reamd <reamdmeng@tencent.com>
2021-04-30 14:26:17 +0800

    feat: first

---
+++ index.js
@@ -0,0 +1,81 @@
+const fetch = require('node-fetch');
+const AbortController = require('abort-controller');
+const Feed = require('feed').Feed;
+const fs = require('fs/promises');
+const process = require('process');
+
+const controller = new AbortController();
+// 30 秒后取消请求
+const timeout = setTimeout(
+  () => { controller.abort(); },
+  30000,
+);
+
+const URL = 'https://api.hmister.cn/weibo';
+
+const feed = new Feed({
+  title: '新浪微博热点',
+  description: '新浪微博TOP50',
+  link: 'https://reamd.github.io/wb-top-feed/',
+  language: 'zh-CN',
+  generator: 'sina weibo feed generator',
+  feedLinks: {
+    json: 'https://reamd.github.io/wb-top-feed/rss.json',
+    rss: 'https://reamd.github.io/wb-top-feed/rss.xml'
+  },
+});
+
+async function main() {
+    const response = await fetch(URL, {
+      headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10130'},
+      signal: controller.signal
+    });
+
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error('wrong status code');
+    }
+
+    const result = await response.json();
+    console.log(`successfully fetch the feed.`);
+
+    if (result?.code !== 200) return;
+
+    const items = result.data;
+    console.log(`successfully parse the feed.`);
+
+    items.forEach(item => {
+      feed.addItem({
+        title: item.name,
+        id: item.id,
+        link: item.url,
+      });
+    });
+
+    console.log(`successfully generating new feed.`);
+
+    await fs.rmdir('./dist', { recursive: true });
+    console.log(`successfully deleted ./dist`);
+
+    await fs.mkdir('./dist');
+    console.log(`successfully create ./dist`);
+
+    await fs.writeFile('./dist/rss.json', feed.json1());
+    console.log(`successfully write rss.json`, feed.items[0]);
+
+    await fs.writeFile('./dist/rss.xml', feed.rss2());
+    console.log(`successfully write rss.xml`);
+
+    await fs.copyFile('./template/index.html', `./dist/index.html`);
+    await fs.copyFile('./template/page.js', `./dist/page.js`);
+    console.log(`successfully copy asset files`);
+
+}
+
+main()
+.catch(err => {
+  console.log(err);
+  process.exit(1);
+})
+.finally(() => {
+  clearTimeout(timeout);
+});
---
+++ template/page.js
@@ -0,0 +1,17 @@
+fetch('./rss.json')
+.then(async function (response) {
+  const res = await response.json();
+  const items = res.items;
+
+  const list = document.querySelector('.list');
+
+  items.forEach(i => {
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const idx = i.id;
+    p.innerHTML = `<a href="${i.url}" target="_blank">【${idx === 0 ? '置顶' : idx}】${i.name}</a>`;
+    li.appendChild(p);
+    list.appendChild(li);
+  });
+})
+
